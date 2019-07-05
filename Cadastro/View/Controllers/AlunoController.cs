dd6baf71939ec0d4b2c9add8ab5284fac440244d
Eduardo Ehmke Hausmann <eduardohausmann1705@gmail.com>
2019-07-05 08:42:21 -0300

    Concluido o AlunoController

--- Cadastro/View/Controllers/AlunoController.cs
+++ Cadastro/View/Controllers/AlunoController.cs
@@ -25,5 +25,44 @@ namespace View.Controllers
         {
             return View();
         }
+
+        public ActionResult Store(string nome, string cpf, decimal nota1, decimal nota2, decimal nota3)
+        {
+            Aluno aluno = new Aluno();
+            aluno.Nome = nome;
+            aluno.Cpf = cpf;
+            aluno.Nota1 = nota1;
+            aluno.Nota2 = nota2;
+            aluno.Nota3 = nota3;
+            int id = repositorio.Inserir(aluno);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            repositorio.Apagar(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Aluno aluno = repositorio.ObterPeloId(id);
+            ViewBag.Aluno = aluno;
+            return View();
+        }
+
+        public ActionResult Update(int id, string nome, string cpf, decimal nota1, decimal nota2, decimal nota3)
+        {
+            Aluno aluno = new Aluno();
+            aluno.Id = id;
+            aluno.Nome = nome;
+            aluno.Cpf = cpf;
+            aluno.Nota1 = nota1;
+            aluno.Nota2 = nota2;
+            aluno.Nota3 = nota3;
+
+            bool alterou = repositorio.Atualizar(aluno);
+            return RedirectToAction("Index");
+        }
     }
 }
